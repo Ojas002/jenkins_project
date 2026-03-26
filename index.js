02ef843fc764d3fd85521a45a7a7458277ecd65f
Ubuntu <ubuntu@ip-172-31-22-233.ec2.internal>
2026-03-26 09:07:46 +0000

    cm git add .

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
-        res.send('Welcome to jenkins server');
+        res.send('Welcome to jenkins server!!');
 });//listen to port 2999 by default
 app.listen(process.env.PORT || 3000);
 
